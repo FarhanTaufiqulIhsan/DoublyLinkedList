9766af2f1613142cce9d5b9b98b960cb2d75d7fd
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2022-11-16 18:24:21 +0700

    Add project files.

---
+++ DoublyLinkedList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedList
+{
+    class Node
+    {
+        /*Node class represents the node of doubly linked list.
+         * It consoscts of the information part and links to
+         * its succeeding and preceeding nodes
+         * in terms od next and previous nodes.*/
+        public int rollNumber;
+        public string name;
+        public Node next;/*points to the suceeding node*/
+        public Node prev;/*points to the preceeding node*/
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
